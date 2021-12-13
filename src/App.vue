b0a6dc190f1f3d5171de01ea178b947a795b62b3
xinwuyun <zrwangxdu@outlook.com>
2021-12-13 21:09:01 +0800

    初步效果

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+      <v-chart class="chart" :option="option" />
+      <div>
+        
+      </div>
+      <codemirror v-model="code" :options="cmOptions" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VChart, { THEME_KEY } from "vue-echarts";
 export default {
-  name: 'App',
+  name: 'app',
+  provide: {
+    [THEME_KEY]: "dark"
+  },
+  data() {
+    return {
+      code: 'const a = 10',
+      cmOptions: {
+        tabSize: 4,
+        mode: 'text/javascript',
+        theme: 'base16-dark',
+        lineNumbers: true,
+        line: true,
+        // more CodeMirror options...
+      },
+      option: {
+        title: {
+          text: "Traffic Sources",
+          left: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: [
+            "Direct",
+            "Email",
+            "Ad Networks",
+            "Video Ads",
+            "Search Engines"
+          ]
+        },
+        series: [
+          {
+            name: "Traffic Sources",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: 335, name: "Direct" },
+              { value: 310, name: "Email" },
+              { value: 234, name: "Ad Networks" },
+              { value: 135, name: "Video Ads" },
+              { value: 1548, name: "Search Engines" }
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    onCmReady(cm) {
+      console.log('the editor is readied!', cm)
+    },
+    onCmFocus(cm) {
+      console.log('the editor is focused!', cm)
+    },
+    onCmCodeChange(newCode) {
+      console.log('this is new code', newCode)
+      this.code = newCode
+    }
+  },
+  computed: {
+    codemirror() {
+      return this.$refs.cmEditor.codemirror
+    }
+  },
+  mounted() {
+    console.log('the current CodeMirror instance object:', this.codemirror)
+    // you can use this.codemirror to do something...
+  },
   components: {
-    HelloWorld
+    VChart
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.chart {
+  height: 400px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ECharts from 'vue-echarts'
+import './plugins/ant-design-vue.js'
+import VueCodemirror from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+import "echarts";
 
 Vue.config.productionTip = false
+Vue.use(VueCodemirror)
+
+Vue.component('v-chart', ECharts)
+
 
 new Vue({
   render: h => h(App),
